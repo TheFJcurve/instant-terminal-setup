0b67ae1bb17f42612578d5b7cdf8d51726cfa37c
Sargun <sargunbhatti27@gmail.com>
2024-05-11 03:52:36 +0800

    check for os

--- start.sh
+++ start.sh
@@ -2,19 +2,14 @@
 
 scp .aliases  ~/.aliases
 
-if [[ "$OSTYPE" =~ ^darwin ]]; then
-    scp .bashrc ~/.zshrc
-    scp .aphrodite.zsh-theme ~/.aphrodite.theme.sh
-    source ~/.zshrc
-else
-    scp .bashrc ~/.
-    scp .aphrodite.theme.sh ~/.aphrodite.theme.sh
-    source ~/.bashrc
-fi
+case "$OSTYPE" in
+  darwin*)  scp .bashrc ~/.zshrc && scp .aphrodite.zsh-theme ~/.aphrodite.theme.sh ;; 
+  *) scp .bashrc ~/.bashrc && scp .aphrodite.theme.sh ~/.aphrodite.theme.sh ;; 
+esac
 
 mkdir -p ~/.ssh 
 CONFIG_FILE=~/.ssh/config
 touch ${CONFIG_FILE}
 echo "Host ss2bhatt" > ${CONFIG_FILE}
-echo "Hostname ubuntu2204-004.student.cs.uwaterloo.ca" >> ${CONFIG_FILE}
+echo "Hostname ubuntu.student.cs.uwaterloo.ca" >> ${CONFIG_FILE}
 echo "User ss2bhatt" >> ${CONFIG_FILE}
